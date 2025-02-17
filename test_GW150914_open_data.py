e1891295c33bf7d22b15bc8042f61390ffe12cc8
Samson Leong (wkstn) <55839002+SSL32081@users.noreply.github.com>
2025-02-17 20:52:18 +0800

    Add working test GW150914 script

--- test_GW150914_open_data.py
+++ test_GW150914_open_data.py
@@ -1,9 +1,8 @@
-## Import Jim-relatred stuff
-
+#!/usr/bin/python3
 import os
 os.environ['JAX_PLATFORMS'] = 'cpu'
 import pickle
-import ast
+from pathlib import Path
 import numpy as np
 
 print("Importing JAX")
@@ -12,36 +11,138 @@ import jax.numpy as jnp
 jax.config.update("jax_enable_x64", True)
 print("Importing JAX successful")
 
-from jimgw.jim import Jim
-from jimgw.single_event.detector import H1, L1, V1, GroundBased2G
-from jimgw.single_event.likelihood import TransientLikelihoodFD, HeterodynedTransientLikelihoodFD
+from jimgw.single_event.detector import H1, L1
+from jimgw.single_event.likelihood import TransientLikelihoodFD
 from jimgw.single_event.waveform import RippleIMRPhenomPv2
 
-from gwpy.timeseries import TimeSeries
+from bilby.gw.result import CBCResult
+from bilby.gw import WaveformGenerator, GravitationalWaveTransient
+from bilby.gw.source import lal_binary_black_hole
 
-# Copy from the GW150914 example from Bilby.
+## We load in the pickle dump from the bilby run
+indir = Path('bilby_runs/outdir/GW150914')
+with open(indir / 'GW150914_data0_1126259462-391_generation_data_dump.pickle', 'rb') as pickled_data:
+    bilby_gen_data = pickle.load(pickled_data)
 
-# Note you can get trigger times using the gwosc package, e.g.:
-# > from gwosc import datasets
-# > datasets.event_gps("GW150914")
-trigger_time = 1126259462.4
-detectors = ["H1", "L1"]
-sampling_frequency = 2048
-maximum_frequency = sampling_frequency / 2
-minimum_frequency = 20
-roll_off = 0.4  # Roll off duration of tukey window in seconds, default is 0.4s
-duration = 4  # Analysis segment duration
-post_trigger_duration = 2  # Time between trigger time and end of segment
-end_time = trigger_time + post_trigger_duration
-start_time = end_time - duration
+bilby_poste = CBCResult.from_hdf5(
+    indir / 'GW150914_data0_1126259462-391_analysis_H1L1_result.hdf5'
+).posterior
 
-psd_duration = 32 * duration
-psd_start_time = start_time - psd_duration
-psd_end_time = start_time
+## Define detectors
+bilby_ifos = bilby_gen_data.interferometers
 
 jim_ifos = [H1, L1]
+for i, jim_ifo in enumerate(jim_ifos):
 
-# Load public data using GWOSC
-for ifo in jim_ifos:
-    time_series = TimeSeries.fetch_open_data(ifo.name, start_time, end_time)
-    strain_start_time = time_series.epoch.value
+    bilby_ifo = bilby_ifos[i]
+    assert bilby_ifo.name == jim_ifo.name, f"{bilby_ifo.name = } != {jim_ifo.name = }"
+
+    freq_mask = bilby_ifo.frequency_mask
+    jim_ifo.frequencies = bilby_ifo.frequency_array[freq_mask]
+    jim_ifo.data = bilby_ifo.frequency_domain_strain[freq_mask]
+    jim_ifo.psd = bilby_ifo.power_spectral_density_array[freq_mask]
+
+sampling_frequency = float(bilby_gen_data.meta_data['command_line_args']['sampling_frequency'])
+reference_frequency = float(bilby_gen_data.meta_data['command_line_args']['reference_frequency'])
+trigger_time = bilby_gen_data.trigger_time
+# Input duration
+duration = float(bilby_gen_data.meta_data['command_line_args']['duration'])
+post_trigger_duration = float(bilby_gen_data.meta_data['command_line_args']['post_trigger_duration'])
+start_time = trigger_time + post_trigger_duration - duration
+print(f"{duration = :.6f}")
+# Precise duration of the strain data
+strain_start_time = bilby_ifos[0].strain_data.start_time
+strain_end_time = bilby_ifos[0].strain_data.time_array[-1]
+strain_duration = strain_end_time - strain_start_time
+strain_post_trigger_duration = strain_end_time - trigger_time
+print(f"{strain_duration = :.6f}")
+
+jim_Pv2 = RippleIMRPhenomPv2(f_ref=reference_frequency)
+
+likelihood_1 = TransientLikelihoodFD(
+    jim_ifos, waveform=jim_Pv2, trigger_time=trigger_time, 
+    duration=duration, post_trigger_duration=post_trigger_duration
+)
+
+likelihood_2 = TransientLikelihoodFD(
+    jim_ifos, waveform=jim_Pv2, trigger_time=trigger_time, 
+    duration=strain_duration, post_trigger_duration=strain_post_trigger_duration
+)
+
+## Reconstruct Bilby waveform generator
+bilby_waveform_generator = WaveformGenerator(
+    duration=duration,
+    sampling_frequency=sampling_frequency,
+    start_time=start_time,
+    frequency_domain_source_model=lal_binary_black_hole,
+    waveform_arguments=dict(
+        waveform_approximant="IMRPhenomPv2",
+        reference_frequency=20,
+        minimum_frequency=20,
+    )
+)
+
+## Reconstruct Bilby likelihood
+likelihood_bilby = GravitationalWaveTransient(
+    interferometers=bilby_ifos,
+    waveform_generator=bilby_waveform_generator
+)
+
+## Start sampling from the bilby posterior samples
+n_samples = 100
+samples_bilby = bilby_poste.sample(n_samples, random_state=42)
+
+keys = ["chirp_mass", "symmetric_mass_ratio", "spin_1x", "spin_1y", "spin_1z", "spin_2x", "spin_2y", "spin_2z", "iota", "luminosity_distance", "phase", "psi", "ra", "dec"]
+jim_keys = ["M_c", "eta", "s1_x", "s1_y", "s1_z", "s2_x", "s2_y", "s2_z", "iota", "d_L", "phase_c", "psi", "ra", "dec"]
+jim_to_bilby_map = dict(zip(jim_keys, keys))
+
+param_list = []
+for i in range(n_samples):
+    sample = samples_bilby.iloc[i].to_dict()
+
+    likelihood_bilby.parameters = sample
+    logL_bilby = likelihood_bilby.log_likelihood_ratio()
+    # print("Bilby Likelihood:", logL_bilby)
+
+    jim_params = {}
+    for key in jim_keys:
+        jim_params[key] = sample[jim_to_bilby_map[key]]
+    jim_params["t_c"] = sample["geocent_time"] - trigger_time
+    logL_jim_1 = likelihood_1.evaluate(jim_params, None)
+    logL_jim_2 = likelihood_2.evaluate(jim_params, None)
+    # print("Jim Likelihood:", logL_jim)
+
+    row = list(jim_params.values())
+    row.append(logL_jim_1)
+    row.append(logL_jim_2)
+    row.append(logL_bilby)
+    param_list.append(tuple(row))
+
+diff_logLs = np.array(param_list, dtype=[(key, "f8") for key in jim_keys + ["t_c", "gps", "logL_jim_1", "logL_jim_2", "logL_bilby"]])
+np.save("compare_GW150914_likelihoods.npy", diff_logLs)
+
+import matplotlib.pyplot as plt
+
+fig, axes = plt.subplots(1, 2, figsize=(3.4 * 2.3, 3.4))
+
+logL_diff_1 = diff_logLs['logL_bilby'] - diff_logLs['logL_jim_1']
+logL_diff_2 = diff_logLs['logL_bilby'] - diff_logLs['logL_jim_2']
+
+ax = axes[0]
+ax.scatter(
+    diff_logLs['logL_bilby'], logL_diff_1, s=5, alpha=0.9
+)
+ax.set_title(f'Use input duration = {duration:.6f} s')
+ax.set_ylabel(r'$ \Delta \ln {\cal L} = \ln {\cal L}_{\rm Bilby} - \ln {\cal L}_{\rm Jim}$')
+
+ax = axes[1]
+ax.scatter(
+    diff_logLs['logL_bilby'], logL_diff_2, s=5, alpha=0.9
+)
+ax.set_title(f'Use strain duration = {strain_duration:.6f} s')
+
+for ax in axes:
+    ax.set_xlabel(r'$\ln {\cal L}_{\rm Bilby}$')
+
+fig.suptitle('GW150914')
+fig.savefig('compare_GW150914_likelihoods.pdf')
